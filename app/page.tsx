71b44807ad3be884f549d59db9c812da76dc2efb
Someone139 <andredinis2011@gmail.com>
2026-05-23 21:24:54 +0100

    Chessboard and home page
    Added a working chessboard with "react-chessboard" and white and black can now move pieces (turn-based) checkmate, checks and draws are detected and move history is displayed. The home page contains a header "Chessverse" and the slogan "Your move." with a "Play now" button underneath.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,139 @@
-import Image from "next/image";
+"use client";
+import { useState } from 'react';
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
 
 export default function Home() {
+  const [started, setStarted] = useState(false);
+  const [game, setGame] = useState(new Chess());
+  const [turn, setTurn] = useState<"w" | "b">("w");
+  const [status, setStatus] = useState<string>("");
+  const [moveHistory, setMoveHistory] = useState<string[]>([]);
+  const [promotionPiece, setPromotionPiece] = useState("q");
+
+  function makeMove(args: {
+    sourceSquare: string;
+    targetSquare: string | null;
+    piece: string;
+  }): boolean {
+    const { sourceSquare, targetSquare } = args;
+
+    if (!targetSquare) return false;
+
+    const isWhitePiece = game.get(sourceSquare as any)?.color === "w";
+
+    if ((turn === "w" && !isWhitePiece) || (turn === "b" && isWhitePiece)) {
+      return false;
+    }
+
+    const gameCopy = new Chess(game.fen());
+
+    const move = gameCopy.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: promotionPiece,
+    });
+
+    if (!move) return false;
+
+    setGame(gameCopy);
+
+    setMoveHistory((prev) => [...prev, move.san]);
+
+    if (gameCopy.isCheckmate()) {
+      setStatus(`Checkmate! ${gameCopy.turn() === "w" ? "Black" : "White"} wins`);
+    }
+    else if (gameCopy.isCheck()) {
+      setStatus(`${gameCopy.turn() === "w" ? "White" : "Black"} is in check`);
+    }
+    else if (gameCopy.isDraw()) {
+      setStatus("Draw");
+    }
+    else {
+      setStatus("");
+    }
+
+    setTurn(turn === "w" ? "b" : "w");
+
+    return true;
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main className="flex min-h-screen bg-zinc-900 text-white justify-center">
+      {!started && (
+        <div className="text-center space-y-1 pt-5">
+          <h1 className="text-5xl font-bold">Chessverse</h1>
+          <p className="text-zinc-400">
+            Your move.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+          onClick={() => setStarted(true)}
+          className="mt-4 inline-block text-center rounded-xl bg-white px-6 py-3 font-semibold text-black hover:bg-zinc-300 transition-colors"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            Play Now
+          </button>
+        </div>
+      )}
+      {started && (
+        <div className="flex flex-col gap-2 min-h-screen items-center justify-center text-white">
+          <div className="w-[400px] text-center">
+            {started && status && (
+              <p className="mb-3 text-yellow-400 font-semibold">
+                {status}
+              </p>
+            )}
+          </div>
+          <div className="mb-4 flex gap-2">
+            <button
+              onClick={() => setPromotionPiece("q")}
+              className="rounded bg-zinc-700 px-3 py-1 hover:bg-zinc-600 transition-colors"
+            >
+              Queen
+            </button>
+
+            <button
+              onClick={() => setPromotionPiece("r")}
+              className="rounded bg-zinc-700 px-3 py-1 hover:bg-zinc-600 transition-colors"
+            >
+              Rook
+            </button>
+
+            <button
+              onClick={() => setPromotionPiece("b")}
+              className="rounded bg-zinc-700 px-3 py-1 hover:bg-zinc-600 transition-colors"
+            >
+              Bishop
+            </button>
+
+            <button
+              onClick={() => setPromotionPiece("n")}
+              className="rounded bg-zinc-700 px-3 py-1 hover:bg-zinc-600 transition-colors"
+            >
+              Knight
+            </button>
+          </div>
+          <div className="w-[400px] shadow-2xl">
+            <Chessboard
+              options={{
+                boardOrientation: "white",
+                position: game.fen(),
+                onPieceDrop: makeMove as any,
+              }}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <div className="mt-4 w-[400px] rounded-xl bg-zinc-800 p-4">
+            <h2 className="mb-2 text-lg font-bold">Move History</h2>
+
+            <div className="grid grid-cols-2 gap-y-1 text-sm text-zinc-300">
+              {moveHistory.map((move, index) => (
+                <div key={index}>
+                  {index + 1}. {move}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+      )}
+    </main>
   );
 }
