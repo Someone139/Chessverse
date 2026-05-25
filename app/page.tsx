20ae213bdf0e0e7d7542bab7e7d614161a3dcded
Someone139 <andredinis2011@gmail.com>
2026-05-25 20:52:56 +0100

    Polished Design
    Polished the design a lot although I am not yet finished. Added a constant "page" (string), replacing "started" which was a boolean so that more pages can be added when buttons are clicked.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@
 import { useState, useEffect } from 'react';
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
+import { Swords, Clock, ChessKing, Puzzle, BookOpen, ChartNoAxesCombined, Trophy, Settings } from "lucide-react";
 
 export default function Home() {
-  const [started, setStarted] = useState(false);
+  const [page, setPage] = useState<string>("Home");
   const [game, setGame] = useState(new Chess());
   const [turn, setTurn] = useState<"w" | "b">("w");
   const [status, setStatus] = useState<string>("");
@@ -62,7 +63,7 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if (gameOver || !started) return;
+    if (gameOver || page !== "Game") return;
 
     const interval = setInterval(() => {
       if (turn === "w") {
@@ -87,28 +88,97 @@ export default function Home() {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [turn, gameOver, started]);
+  }, [turn, gameOver, page]);
 
   return (
-    <main className="flex min-h-screen bg-zinc-900 text-white justify-center">
-      {!started && (
-        <div className="text-center space-y-1 pt-5">
-          <h1 className="text-5xl font-bold">Chessverse</h1>
-          <p className="text-zinc-400">
-            Your move.
-          </p>
-          <button
-          onClick={() => setStarted(true)}
-          className="mt-4 inline-block text-center rounded-xl bg-white px-6 py-3 font-semibold text-black hover:bg-zinc-300 transition-colors"
-          >
-            Play Now
-          </button>
+    <main className="flex min-h-screen text-white justify-center">
+      {page === "Home" && (
+        <div className="min-h-screen min-w-screen grid grid-rows-[60px_1fr]">
+          {/* Top Bar */}
+          <header className="flex justify-between items-center gap-2 min-w-screen px-4 h-[60px] border-b-2 border-zinc-950 bg-gradient-to-b from-[#412B6B]/80 to-purple-950/90">
+            <div className="flex items-center ml-2">
+              {/* Button to home page */}
+              <button
+              onClick={() => setPage("Home")}
+              className="flex gap-1.5 cursor-pointer py-3 px-1 rounded-lg"
+              >
+                <ChessKing size={30} className="-mt-0.5"/>
+                <div className="font-bold text-2xl">
+                  Chessverse
+                </div>
+              </button>
+            </div>
+            {/* Other buttons */}
+            <div className="flex items-center gap-5 mr-10">
+              {/* Play button */}
+              <button
+              onClick={() => setPage("Game")}
+              className="flex cursor-pointer gap-1 bg-[#211832]/90 rounded-lg px-6 py-3"
+              >
+                <Swords size={18} className="mt-[5px]"/>
+                <p className="text-lg font-semibold">Play</p>
+              </button>
+              {/* Puzzles */}
+              <button
+              onClick={() => setPage("Home")}
+              className="flex cursor-pointer gap-1 bg-[#211832]/90 rounded-lg px-6 py-3"
+              >
+                <Puzzle size={18} className="mt-[4px]"/>
+                <p className="text-lg font-semibold">Puzzles</p>
+              </button>
+              {/* Learn */}
+              <button
+              onClick={() => setPage("Home")}
+              className="flex cursor-pointer gap-1 bg-[#211832]/90 rounded-lg px-6 py-3"
+              >
+                <BookOpen size={18} className="mt-[5px]"/>
+                <p className="text-lg font-semibold">Learn</p>
+              </button>
+              {/* Stats */}
+              <button
+              onClick={() => setPage("Home")}
+              className="flex cursor-pointer gap-1 bg-[#211832]/90 rounded-lg px-6 py-3"
+              >
+                <ChartNoAxesCombined size={18} className="mt-[4px]"/>
+                <p className="text-lg font-semibold">Statistics</p>
+              </button>
+              {/* Leaderboard */}
+              <button
+              onClick={() => setPage("Home")}
+              className="flex cursor-pointer gap-1 bg-[#211832]/90 rounded-lg px-6 py-3"
+              >
+                <Trophy size={18} className="mt-[4px]"/>
+                <p className="text-lg font-semibold">Leaderboard</p>
+              </button>
+              {/* Settings */}
+              <button
+              onClick={() => setPage("Home")}
+              className="flex cursor-pointer gap-1 bg-[#211832]/90 rounded-lg px-6 py-3"
+              >
+                <Settings size={18} className="mt-[4px]"/>
+                <p className="text-lg font-semibold">Settings</p>
+              </button>
+            </div>
+
+          </header>
+          <div className="text-center space-y-1 pt-5 bg-gradient-to-b from-[#211832]/80 to-zinc-900/90">
+            <h1 className="text-5xl font-bold">Hello, this website is under development please save and check again later.</h1>
+            <p className="text-zinc-400">
+              Your move.
+            </p>
+            <button
+            onClick={() => setPage("Game")}
+            className="mt-4 inline-block text-center rounded-xl bg-white px-6 py-3 font-semibold text-black hover:bg-zinc-300 transition-colors"
+            >
+              Play Now
+            </button>
+          </div>
         </div>
       )}
-      {started && (
-        <div className="flex flex-col gap-2 min-h-screen items-center justify-center text-white">
+      {page === "Game" && (
+        <div className="flex flex-col gap-2 min-h-screen min-w-screen items-center justify-center text-white bg-zinc-900">
           <div className="w-[400px] text-center">
-            {started && status && (
+            {page === "Game" && status && (
               <p className="mb-3 text-yellow-400 font-semibold">
                 {status}
               </p>
